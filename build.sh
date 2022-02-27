2f3e841186ee7d50bc1dfd43087872daf522d0ba
hashitha <hashitha@sigmalogic.com.au>
2022-02-27 17:04:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
-./dotnet6/dotnet --version
+#curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+#chmod +x dotnet-install.sh
+#./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
+#./dotnet6/dotnet --version
 ls
-./dotnet6/dotnet publish -c Release -o output
+#./dotnet6/dotnet publish -c Release -o output
+npm install --global yarn
+yarn --version
+yarn config set workspaces-experimental true
+yarn install
+yarn run build
